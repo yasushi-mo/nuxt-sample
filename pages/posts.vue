4567c3419d15098455e7715d7703204b7cde1e03
Yasushi Mo <45203052+yasushi-mo@users.noreply.github.com>
2025-05-30 09:12:37 +0900

    Add: useFetch

---
+++ pages/posts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Post</h1>
+    <ul>
+      <li v-for="post in posts" :key="post.id">{{ post.title }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+const { data: posts } = await useFetch(
+  "https://jsonplaceholder.typicode.com/posts"
+);
+</script>
